3dcd73963715e8043c56381c7c2ece1e8221c2f9
Qin Chen <qinchencq@gmail.com>
2014-08-26 18:27:24 -0700

    add new css to control iPhone landscape

--- inc/header.php
+++ inc/header.php
@@ -4,7 +4,7 @@
 	<head>
                 <meta charset="utf-8">
                 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
                 
                 <title>Drugging Our Kids | San Jose Mercury News</title>
                 <meta name="description" content="California’s foster-care system is giving thousands of kids psychotropic drugs instead of providing stable homes and loving families." />
